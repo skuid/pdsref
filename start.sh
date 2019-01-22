748caf08655cffd6c0b1b9deecbe6ee61a90b978
Brian Newton <brian.newton@skuid.com>
2019-01-22 13:50:38 -0500

    Adding clortho, doing a little bit of splitting, updating versions

--- start.sh
+++ start.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-printf "Would you like to start the local postgres image? [y|N] "
+printf "Would you like to start the local postgres images? [y|N] "
 read startpg
 
 if [ "${startpg}" == "y" ]
 then
   source env/warden
-  docker-compose up -d postgres
+  docker-compose up -d -f docker-compose-dbs.yml
+
+  while ! ./ready.sh
+  do
+    sleep 5
+  done
+
+  source env/clortho
 
   while ! ./ready.sh
   do
@@ -16,4 +23,5 @@ fi
 
 docker-compose up -d redis seaquill
 docker-compose run --rm warden migrate up
-docker-compose up -d warden sluice
+docker-compose run --rm clortho migrate up
+docker-compose up -d clortho warden sluice
